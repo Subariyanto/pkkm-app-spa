0d6b7e8d9b32cccbb390b8c1a332d4b34e795c06
Subariyanto <subariyantoss2@gmail.com>
2026-05-28 09:39:47 +0700

    ui(brand): rata kiri 'Pokjawasmad Kab. Jember' sejajar dengan huruf 'A' Aplikasi (padding-left 1.55rem untuk skip lebar icon mortarboard)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service worker for PKKM SPA
 // Strategy: network-first for app code, cache-first for static assets.
-const CACHE_VERSION = 'pkkm-v2-2026-05-28-r22';
+const CACHE_VERSION = 'pkkm-v2-2026-05-28-r23';
 
 const NETWORK_FIRST = [
   'index.html',
